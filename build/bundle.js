469fa2fecf1728a670a98e190d2dfb226ecd27ad
Peter Weinberg <pweinberg633@gmail.com>
2016-12-28 13:56:01 -0500

    fix portfolio & landing page assert

--- build/bundle.js
+++ build/bundle.js
@@ -19888,37 +19888,37 @@ var FCC_Global =
 
 	    describe('#Content', function () {
 	      it('1. My portfolio should have a Welcome section with an id of \"welcome-section\".', function () {
-	        assert.isNotNull(document.getElementById('welcome-section'));
+	        FCC_Global.assert.isNotNull(document.getElementById('welcome-section'));
 	      });
 
 	      it('2. The welcome section should have an h1 element that contains text.', function () {
 	        var welcome = document.getElementById('welcome-section');
 
-	        assert.isAbove(document.querySelectorAll('#welcome-section h1').length, 0, 'Welcome section should contain an h1 element ');
+	        FCC_Global.assert.isAbove(document.querySelectorAll('#welcome-section h1').length, 0, 'Welcome section should contain an h1 element ');
 
-	        assert.isAbove(document.querySelectorAll('#welcome-section h1')[0].innerText.length, 0, 'h1 element in welcome section should contain your name or camper name ');
+	        FCC_Global.assert.isAbove(document.querySelectorAll('#welcome-section h1')[0].innerText.length, 0, 'h1 element in welcome section should contain your name or camper name ');
 	      });
 
 	      it('3. My portfolio should have a projects section with an id of \"projects\".', function () {
-	        assert.isNotNull(document.getElementById('projects'));
+	        FCC_Global.assert.isNotNull(document.getElementById('projects'));
 	      });
 
 	      it('4. The projects section should contain at least one element with a class of \"project-tile\" to hold a project.', function () {
-	        assert.isAbove(document.querySelectorAll('#projects .project-tile').length, 0);
+	        FCC_Global.assert.isAbove(document.querySelectorAll('#projects .project-tile').length, 0);
 	      });
 
 	      it('5. The projects section should contain at least one link to a project.', function () {
-	        assert.isAbove(document.querySelectorAll('#projects a').length, 0);
+	        FCC_Global.assert.isAbove(document.querySelectorAll('#projects a').length, 0);
 	      });
 
 	      it('6. My portfolio should have a navbar with an id of \"navbar\".', function () {
 	        var navbar = document.getElementById('navbar');
 
-	        assert.isNotNull(navbar);
+	        FCC_Global.assert.isNotNull(navbar);
 	      });
 
 	      it('7. The navbar should contain at least one link that I can click on to navigate to different sections of the page.', function () {
-	        assert.isAbove(document.querySelectorAll('#navbar a').length, 0, 'Navbar should contain a link ');
+	        FCC_Global.assert.isAbove(document.querySelectorAll('#navbar a').length, 0, 'Navbar should contain a link ');
 
 	        var links = document.querySelectorAll('#navbar a');
 
@@ -19927,7 +19927,7 @@ var FCC_Global =
 
 	          if (window.scrollY) {
 	            // if the window's y position is not 0, clicking a link made it move
-	            assert.ok(true);
+	            FCC_Global.assert.ok(true);
 	            window.scroll(0, 0);
 	            return;
 	          }
@@ -19944,7 +19944,7 @@ var FCC_Global =
 
 	          if (distanceFromBottom) {
 	            // if distance from bottom is not 0, clicking a link made it move
-	            assert.ok(true);
+	            FCC_Global.assert.ok(true);
 	            window.scroll(0, 0);
 	            return;
 	          }
@@ -19952,19 +19952,19 @@ var FCC_Global =
 
 	        // none of the links changed the scroll position
 	        window.scroll(0, 0);
-	        assert.isOk(false, 'At least one navbar link should move the page position when clicked ');
+	        FCC_Global.assert.isOk(false, 'At least one navbar link should move the page position when clicked ');
 	      });
 
 	      it('8. My portfolio should have a link with an id of \"profile-link\", which opens my GitHub or FCC profile in a new tab.', function () {
 	        var profileLink = document.getElementById('profile-link');
 
-	        assert.isNotNull(profileLink);
+	        FCC_Global.assert.isNotNull(profileLink);
 
-	        assert.equal(profileLink.nodeName, 'A');
+	        FCC_Global.assert.equal(profileLink.nodeName, 'A');
 
-	        assert.strictEqual(profileLink.hasAttribute('target'), true, '#profile-link should have a target attribute ');
+	        FCC_Global.assert.strictEqual(profileLink.hasAttribute('target'), true, '#profile-link should have a target attribute ');
 
-	        assert.equal(profileLink.target, '_blank', 'Clicking #profile-link should cause a link to open in a new tab ');
+	        FCC_Global.assert.equal(profileLink.target, '_blank', 'Clicking #profile-link should cause a link to open in a new tab ');
 	      });
 	    }); // END #Content
 
@@ -19976,25 +19976,25 @@ var FCC_Global =
 	          if (document.styleSheets[i].cssRules !== null) {
 	            for (var j = 0; j < document.styleSheets[i].cssRules.length; j++) {
 	              if (document.styleSheets[i].cssRules[j].type === 4) {
-	                assert.isOk(true);
+	                FCC_Global.assert.isOk(true);
 	                return;
 	              }
 	            }
 	          }
 	        }
 	        // if no media query is detected, test fails
-	        assert.isOk(false, "No media queries detected ");
+	        FCC_Global.assert.isOk(false, "No media queries detected ");
 	      });
 
 	      it('2. The height of the welcome section should be equal to the height of the viewport.', function () {
-	        assert.equal(document.getElementById('welcome-section').offsetHeight, document.documentElement.clientHeight, 'The height of #welcome-section is not equal to the height of the viewport ');
+	        FCC_Global.assert.equal(document.getElementById('welcome-section').offsetHeight, document.documentElement.clientHeight, 'The height of #welcome-section is not equal to the height of the viewport ');
 	      });
 
 	      it('3. The navbar should always be at the top of the viewport.', function () {
 	        var navbar = document.getElementById('navbar');
-	        assert.approximately(navbar.getBoundingClientRect().top, 0, 15), 'Navbar\'s parent should be body and it should be at the top of the viewport ';
+	        FCC_Global.assert.approximately(navbar.getBoundingClientRect().top, 0, 15), 'Navbar\'s parent should be body and it should be at the top of the viewport ';
 	        window.scroll(0, 500);
-	        assert.approximately(navbar.getBoundingClientRect().top, 0, 15, 'Navbar should be at the top of the viewport even after scrolling ');
+	        FCC_Global.assert.approximately(navbar.getBoundingClientRect().top, 0, 15, 'Navbar should be at the top of the viewport even after scrolling ');
 	        window.scroll(0, 0);
 	      });
 	    }); // END #Layout
@@ -20017,86 +20017,86 @@ var FCC_Global =
 	    describe("#Content", function () {
 
 	      it('1. My product landing page should have a <header> element with corresponding id="header".', function () {
-	        assert.isNotNull(document.getElementById("header"), '#header is not defined ');
+	        FCC_Global.assert.isNotNull(document.getElementById("header"), '#header is not defined ');
 	      });
 
 	      it('2. I can see an image within the #header element with a corresponding id="header-img".', function () {
 	        var img = document.getElementById("header-img");
-	        assert.isNotNull(img, '#header-img is not defined ');
-	        assert.strictEqual(img.nodeName, 'IMG', '#header-img is not an <img> element ');
-	        assert.strictEqual(document.querySelectorAll('#header #header-img').length, 1, '#header-img is not a child of #header ');
-	        assert.strictEqual(img.hasAttribute('src'), true, '#header-img must have a src attribute ');
-	        assert.include(img.src, 'http', "The src attribute\'s value should be a url (http...) ");
+	        FCC_Global.assert.isNotNull(img, '#header-img is not defined ');
+	        FCC_Global.assert.strictEqual(img.nodeName, 'IMG', '#header-img is not an <img> element ');
+	        FCC_Global.assert.strictEqual(document.querySelectorAll('#header #header-img').length, 1, '#header-img is not a child of #header ');
+	        FCC_Global.assert.strictEqual(img.hasAttribute('src'), true, '#header-img must have a src attribute ');
+	        FCC_Global.assert.include(img.src, 'http', "The src attribute\'s value should be a url (http...) ");
 	      });
 
 	      it('3. Within the <header> element I can see a <nav> element with corresponding id="nav-bar".', function () {
-	        assert.isNotNull(document.getElementById("nav-bar"), '#nav-bar is not defined ');
-	        assert.strictEqual(document.getElementById("nav-bar").nodeName, 'NAV', 'The #nav-bar element is not a <nav> element ');
-	        assert.strictEqual(document.querySelectorAll('#header #nav-bar').length, 1, '#nav-bar is not a child of #header ');
+	        FCC_Global.assert.isNotNull(document.getElementById("nav-bar"), '#nav-bar is not defined ');
+	        FCC_Global.assert.strictEqual(document.getElementById("nav-bar").nodeName, 'NAV', 'The #nav-bar element is not a <nav> element ');
+	        FCC_Global.assert.strictEqual(document.querySelectorAll('#header #nav-bar').length, 1, '#nav-bar is not a child of #header ');
 	      });
 
 	      it('4. I can see at least three clickable elements inside the nav element, each with the class "nav-link".', function () {
-	        assert.isAtLeast(document.querySelectorAll('#nav-bar .nav-link').length, 3, 'There are not at least 3 elements with a class of "nav-link" within the #nav-bar element ');
+	        FCC_Global.assert.isAtLeast(document.querySelectorAll('#nav-bar .nav-link').length, 3, 'There are not at least 3 elements with a class of "nav-link" within the #nav-bar element ');
 	      });
 
 	      it('5. When I click a .nav-link button in the nav element, I am taken to the corresponding section of the landing page.', function () {
 	        var navLinks = document.querySelectorAll('#nav-bar .nav-link');
-	        assert.isAtLeast(document.querySelectorAll('#nav-bar .nav-link').length, 3, 'There are not at least 3 elements with a class of "nav-link" ');
+	        FCC_Global.assert.isAtLeast(document.querySelectorAll('#nav-bar .nav-link').length, 3, 'There are not at least 3 elements with a class of "nav-link" ');
 	        navLinks.forEach(function (link) {
-	          assert.isNotNull(link);
-	          assert.strictEqual(link.hasAttribute('href'), true, 'Each .nav-link element should have an href attribute ');
+	          FCC_Global.assert.isNotNull(link);
+	          FCC_Global.assert.strictEqual(link.hasAttribute('href'), true, 'Each .nav-link element should have an href attribute ');
 	          var linkDestination = link.getAttribute('href').slice(1);
-	          assert.isNotNull(document.getElementById(linkDestination), 'The .nav-link with href="' + link.getAttribute('href') + '" is not linked to a corresponding element ');
+	          FCC_Global.assert.isNotNull(document.getElementById(linkDestination), 'The .nav-link with href="' + link.getAttribute('href') + '" is not linked to a corresponding element ');
 	        });
 	      });
 
 	      it('6. I can watch an embedded product video with id="video".', function () {
 	        var video = document.getElementById("video");
-	        assert.isNotNull(video, '#video is not defined ');
-	        assert(video.nodeName === 'VIDEO' || video.nodeName === 'IFRAME', '#video should be an <iframe> or <video> element ');
-	        assert.strictEqual(video.hasAttribute('src'), true, '#video should have a scr attribute ');
+	        FCC_Global.assert.isNotNull(video, '#video is not defined ');
+	        FCC_Global.assert(video.nodeName === 'VIDEO' || video.nodeName === 'IFRAME', '#video should be an <iframe> or <video> element ');
+	        FCC_Global.assert.strictEqual(video.hasAttribute('src'), true, '#video should have a scr attribute ');
 	      });
 
 	      it('7. My landing page has a <form> element with a corresponding id="form".', function () {
-	        assert.isNotNull(document.getElementById('form'), '#form is not defined ');
-	        assert.strictEqual(document.getElementById('form').nodeName, 'FORM', '#form should be a <form> element ');
+	        FCC_Global.assert.isNotNull(document.getElementById('form'), '#form is not defined ');
+	        FCC_Global.assert.strictEqual(document.getElementById('form').nodeName, 'FORM', '#form should be a <form> element ');
 	      });
 
 	      it('8. Within the form, there is an <input> field with id="email" where I can enter an email address.', function () {
-	        assert.isNotNull(document.getElementById("email"), '#email is not defined ');
-	        assert.strictEqual(document.querySelectorAll('#form #email').length, 1, '#email should be a child of the #form element ');
-	        assert.strictEqual(document.getElementById("email").nodeName, 'INPUT', '#email should be an <input> element ');
+	        FCC_Global.assert.isNotNull(document.getElementById("email"), '#email is not defined ');
+	        FCC_Global.assert.strictEqual(document.querySelectorAll('#form #email').length, 1, '#email should be a child of the #form element ');
+	        FCC_Global.assert.strictEqual(document.getElementById("email").nodeName, 'INPUT', '#email should be an <input> element ');
 	      });
 
 	      it('9. The #email input field should have placeholder text to let the user know what the field is for.', function () {
-	        assert.isNotNull(document.getElementById("email"), '#email is not defined ');
-	        assert.strictEqual(document.getElementById("email").hasAttribute('placeholder'), true, 'The input field does not have placehoder text ');
-	        assert.isAbove(document.getElementById("email").getAttribute('placeholder').length, 0, 'The placeholder attribute should have some text ');
+	        FCC_Global.assert.isNotNull(document.getElementById("email"), '#email is not defined ');
+	        FCC_Global.assert.strictEqual(document.getElementById("email").hasAttribute('placeholder'), true, 'The input field does not have placehoder text ');
+	        FCC_Global.assert.isAbove(document.getElementById("email").getAttribute('placeholder').length, 0, 'The placeholder attribute should have some text ');
 	      });
 
 	      it('10. The #email input field uses HTML5 validation to confirm that the entered text is an email address.', function () {
 	        var emailField = document.getElementById("email");
-	        assert.isNotNull(document.getElementById("email"), '#email is not defined ');
-	        assert.strictEqual(emailField.type, "email", "Email field should use HTML5 validation ");
+	        FCC_Global.assert.isNotNull(document.getElementById("email"), '#email is not defined ');
+	        FCC_Global.assert.strictEqual(emailField.type, "email", "Email field should use HTML5 validation ");
 	      });
 
 	      it('11. Within the form, there is a submit <input> with corresponding id="submit".', function () {
 	        var submitButton = document.getElementById("submit");
-	        assert.isNotNull(submitButton, '#submit is not defined ');
-	        assert.strictEqual(document.querySelectorAll('#form #submit').length, 1, '#submit should be a child of the #form element ');
-	        assert.strictEqual(submitButton.nodeName, 'INPUT', '#email should be an <input> element ');
-	        assert.strictEqual(submitButton.type, 'submit', 'The input type is incorrect ');
+	        FCC_Global.assert.isNotNull(submitButton, '#submit is not defined ');
+	        FCC_Global.assert.strictEqual(document.querySelectorAll('#form #submit').length, 1, '#submit should be a child of the #form element ');
+	        FCC_Global.assert.strictEqual(submitButton.nodeName, 'INPUT', '#email should be an <input> element ');
+	        FCC_Global.assert.strictEqual(submitButton.type, 'submit', 'The input type is incorrect ');
 	      });
 
 	      it('12. When I click the #submit button, the email is submitted to a static page (use this mock URL: https://www.freecodecamp.com/email-submit) that confirms the email address was entered (and that it posted successfully).', function () {
 	        var emailField = document.getElementById("email");
 	        var form = document.getElementById('form');
 	        var submitButton = document.getElementById('submit');
-	        assert.isNotNull(submitButton, '#submit is not defined ');
-	        assert.strictEqual(form.hasAttribute('action'), true, 'The #form should have an action attribute ');
-	        assert.include(form.action, 'http', "The action attribute\'s value should be a url (http...) ");
-	        assert.strictEqual(emailField.hasAttribute('name'), true, 'The #email input should have a name attribute ');
-	        assert.strictEqual(emailField.name, "email", 'The name attribute should have a value of "email" ');
+	        FCC_Global.assert.isNotNull(submitButton, '#submit is not defined ');
+	        FCC_Global.assert.strictEqual(form.hasAttribute('action'), true, 'The #form should have an action attribute ');
+	        FCC_Global.assert.include(form.action, 'http', "The action attribute\'s value should be a url (http...) ");
+	        FCC_Global.assert.strictEqual(emailField.hasAttribute('name'), true, 'The #email input should have a name attribute ');
+	        FCC_Global.assert.strictEqual(emailField.name, "email", 'The name attribute should have a value of "email" ');
 	      });
 	    }); // END #Content
 
@@ -20104,10 +20104,10 @@ var FCC_Global =
 	      it('1. The <header> element with id="header" should always be at the top of the viewport.', function () {
 	        var header = document.getElementById('header');
 
-	        assert.approximately(header.getBoundingClientRect().top, 0, 15, '#header\'s parent should be body and it should be at the top of the viewport ');
+	        FCC_Global.assert.approximately(header.getBoundingClientRect().top, 0, 15, '#header\'s parent should be body and it should be at the top of the viewport ');
 
 	        window.scroll(0, 500);
-	        assert.approximately(header.getBoundingClientRect().top, 0, 15, '#header should be at the top of the viewport even after scrolling ');
+	        FCC_Global.assert.approximately(header.getBoundingClientRect().top, 0, 15, '#header should be at the top of the viewport even after scrolling ');
 	        window.scroll(0, 0);
 	      });
 
@@ -20117,14 +20117,14 @@ var FCC_Global =
 	          if (document.styleSheets[i].cssRules !== null) {
 	            for (var j = 0; j < document.styleSheets[i].cssRules.length; j++) {
 	              if (document.styleSheets[i].cssRules[j].type === 4) {
-	                assert.isOk(true);
+	                FCC_Global.assert.isOk(true);
 	                return;
 	              }
 	            }
 	          }
 	        }
 	        // if no media query is detected, test fails
-	        assert.isOk(false, "No media queries detected ");
+	        FCC_Global.assert.isOk(false, "No media queries detected ");
 	      });
 
 	      it('3. My product landing page should utilize CSS flexbox at least once.', function () {
@@ -20141,7 +20141,7 @@ var FCC_Global =
 	        }
 	        // our test suite uses a display of flex, so we need to count how many times its used 
 	        // and confirm that its more than once. If we just detect one instance, its ours. 
-	        assert.isAbove(flexCount.length, 1, 'We do not detect a display property set to flex or inline-flex anywhere in your CSS ');
+	        FCC_Global.assert.isAbove(flexCount.length, 1, 'We do not detect a display property set to flex or inline-flex anywhere in your CSS ');
 	      });
 	    }); // END #Layout
 	  }); // END #ProductLadingPageTests
--- src/project-tests/portfolioTests.js
+++ src/project-tests/portfolioTests.js
@@ -4,37 +4,37 @@ export default function createPortfolioTests() {
     
     describe('#Content', function() {
       it('1. My portfolio should have a Welcome section with an id of \"welcome-section\".', function() {
-        assert.isNotNull(document.getElementById('welcome-section'));
+        FCC_Global.assert.isNotNull(document.getElementById('welcome-section'));
       })
       
       it('2. The welcome section should have an h1 element that contains text.', function() {
         const welcome = document.getElementById('welcome-section')
 
-        assert.isAbove(document.querySelectorAll('#welcome-section h1').length, 0, 'Welcome section should contain an h1 element ');
+        FCC_Global.assert.isAbove(document.querySelectorAll('#welcome-section h1').length, 0, 'Welcome section should contain an h1 element ');
 
-        assert.isAbove(document.querySelectorAll('#welcome-section h1')[0].innerText.length, 0, 'h1 element in welcome section should contain your name or camper name ');
+        FCC_Global.assert.isAbove(document.querySelectorAll('#welcome-section h1')[0].innerText.length, 0, 'h1 element in welcome section should contain your name or camper name ');
       })
       
       it('3. My portfolio should have a projects section with an id of \"projects\".', function() {
-        assert.isNotNull(document.getElementById('projects'));
+        FCC_Global.assert.isNotNull(document.getElementById('projects'));
       });
       
       it('4. The projects section should contain at least one element with a class of \"project-tile\" to hold a project.', function() {
-        assert.isAbove(document.querySelectorAll('#projects .project-tile').length, 0);
+        FCC_Global.assert.isAbove(document.querySelectorAll('#projects .project-tile').length, 0);
       });
       
       it('5. The projects section should contain at least one link to a project.', function() {
-        assert.isAbove(document.querySelectorAll('#projects a').length, 0);
+        FCC_Global.assert.isAbove(document.querySelectorAll('#projects a').length, 0);
       });
       
       it('6. My portfolio should have a navbar with an id of \"navbar\".', function() {
         const navbar = document.getElementById('navbar');
 
-        assert.isNotNull(navbar);
+        FCC_Global.assert.isNotNull(navbar);
       });
       
       it('7. The navbar should contain at least one link that I can click on to navigate to different sections of the page.', function() {
-        assert.isAbove(document.querySelectorAll('#navbar a').length, 0, 'Navbar should contain a link ');
+        FCC_Global.assert.isAbove(document.querySelectorAll('#navbar a').length, 0, 'Navbar should contain a link ');
 
         const links = document.querySelectorAll('#navbar a');
 
@@ -43,7 +43,7 @@ export default function createPortfolioTests() {
 
           if(window.scrollY) {
             // if the window's y position is not 0, clicking a link made it move
-            assert.ok(true);
+            FCC_Global.assert.ok(true);
             window.scroll(0, 0);
             return;
           }
@@ -60,7 +60,7 @@ export default function createPortfolioTests() {
 
           if(distanceFromBottom) {
             // if distance from bottom is not 0, clicking a link made it move
-            assert.ok(true);
+            FCC_Global.assert.ok(true);
             window.scroll(0, 0);
             return;
           }
@@ -68,20 +68,20 @@ export default function createPortfolioTests() {
 
         // none of the links changed the scroll position
         window.scroll(0, 0);
-        assert.isOk(false, 'At least one navbar link should move the page position when clicked ');
+        FCC_Global.assert.isOk(false, 'At least one navbar link should move the page position when clicked ');
 
       });
       
       it('8. My portfolio should have a link with an id of \"profile-link\", which opens my GitHub or FCC profile in a new tab.', function() {
         const profileLink = document.getElementById('profile-link');
 
-        assert.isNotNull(profileLink);
+        FCC_Global.assert.isNotNull(profileLink);
   
-        assert.equal(profileLink.nodeName, 'A');
+        FCC_Global.assert.equal(profileLink.nodeName, 'A');
         
-        assert.strictEqual(profileLink.hasAttribute('target'), true, '#profile-link should have a target attribute ');
+        FCC_Global.assert.strictEqual(profileLink.hasAttribute('target'), true, '#profile-link should have a target attribute ');
         
-        assert.equal(profileLink.target, '_blank', 'Clicking #profile-link should cause a link to open in a new tab ');
+        FCC_Global.assert.equal(profileLink.target, '_blank', 'Clicking #profile-link should cause a link to open in a new tab ');
 
       });
       
@@ -95,26 +95,26 @@ export default function createPortfolioTests() {
           if(document.styleSheets[i].cssRules !== null) {
             for(var j = 0; j < document.styleSheets[i].cssRules.length; j++) {
               if(document.styleSheets[i].cssRules[j].type === 4) {
-                assert.isOk(true);
+                FCC_Global.assert.isOk(true);
                 return;
               }
             }
           }
         }
         // if no media query is detected, test fails
-        assert.isOk(false, "No media queries detected ");
+        FCC_Global.assert.isOk(false, "No media queries detected ");
       });
       
       it('2. The height of the welcome section should be equal to the height of the viewport.', function() {
-        assert.equal(document.getElementById('welcome-section').offsetHeight, document.documentElement.clientHeight, 
+        FCC_Global.assert.equal(document.getElementById('welcome-section').offsetHeight, document.documentElement.clientHeight, 
         'The height of #welcome-section is not equal to the height of the viewport ');
       });
       
       it('3. The navbar should always be at the top of the viewport.', function() {
         const navbar = document.getElementById('navbar');
-        assert.approximately(navbar.getBoundingClientRect().top, 0, 15), 'Navbar\'s parent should be body and it should be at the top of the viewport ';
+        FCC_Global.assert.approximately(navbar.getBoundingClientRect().top, 0, 15), 'Navbar\'s parent should be body and it should be at the top of the viewport ';
         window.scroll(0, 500);        
-        assert.approximately(navbar.getBoundingClientRect().top, 0, 15, 'Navbar should be at the top of the viewport even after scrolling ');
+        FCC_Global.assert.approximately(navbar.getBoundingClientRect().top, 0, 15, 'Navbar should be at the top of the viewport even after scrolling ');
         window.scroll(0, 0); 
       });
       
