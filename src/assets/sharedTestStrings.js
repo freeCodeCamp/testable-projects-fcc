a1dc7df375397c837c4630d4669fa3e755ec6732
Todd Chaffee <tc@bluecrow.biz>
2017-08-22 15:19:06 -0300

    Added the tech stack requirement and eliminated all ESLint warnings.

--- src/assets/sharedTestStrings.js
+++ src/assets/sharedTestStrings.js
@@ -1,4 +1,4 @@
-export const responsiveWebDesignStack = '0. You can use HTML, JavaScript, ' +
+export const responsiveWebDesignStack = '1. You can use HTML, JavaScript, ' +
   'and CSS to complete this project. Plain CSS is recommended because that ' +
   'is what the lessons have covered so far and you should get some practice ' +
   'with plain CSS. You can use Bootstrap or SASS if you choose. Additional ' +
