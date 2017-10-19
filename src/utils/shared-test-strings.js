8d02921bbab5bbbf6b6b11ed6176a8315090a89a
tbushman <tbushman@pu.bli.sh>
2017-10-19 14:07:29 -0600

    Scatter plot tech stack

--- src/utils/shared-test-strings.js
+++ src/utils/shared-test-strings.js
@@ -17,3 +17,16 @@ export const frontEndLibrariesStack = '1. You can use any mix of HTML, ' +
   'frameworks like Angular and Vue, but they are not currently supported. ' +
   'We will accept and try to fix all issue reports that use the suggested ' +
   'technology stack for this project. Happy coding!';
+
+export const d3ProjectStack = '1. You can use HTML, JavaScript, CSS, and the ' +
+  'D3 svg-based visualization library. The tests require axes to be ' +
+  'generated using the D3 axis property, which automatically generates ' +
+  'ticks along the axis. These ticks are required for passing the D3 tests ' +
+  'because their positions are used to determine alignment of graphed ' +
+  'elements. You will find information about generating axes at ' +
+  'https://github.com/d3/d3/blob/master/API.md#axes-d3-axis. Required ' +
+  '(non-virtual) DOM elements are queried on the moment of each test. If ' +
+  'you use a frontend framework (like Vue for example), the test results ' +
+  'may be inaccurate for dynamic content. We hope to accommodate them ' +
+  'eventually, but these frameworks are not currently supported for D3 ' +
+  'projects.';
