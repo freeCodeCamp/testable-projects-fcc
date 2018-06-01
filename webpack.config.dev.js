e35fda313122c4fc63e9457283fe29adf7aac322
Stuart Taylor <staylor1012@gmail.com>
2018-06-01 22:04:44 +0100

    feat: use yarn instead of npm and update webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,35 +3,38 @@ const path = require('path');
 const PROJECT_PATH = path.join(__dirname, '/local_test');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        library: 'FCC_Global',
-        path: path.join(PROJECT_PATH, '/js'),
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['env']
-                }
-            },
-            {
-              test: /\.css$/,
-              exclude: /node_modules/,
-              // chained loaders:
-              // style-loader injects css imported by css-loader
-              loader: 'style-loader!css-loader'
-            },
-            {
-              test: /\.html$/,
-              exclude: /node_modules/,
-              // creates String for importing into JS
-              loader: 'html-loader'
-            }
-        ]
-    }
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		library: 'FCC_Global',
+		path: path.join(PROJECT_PATH, '/js'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+		{
+			test: /\.js$/,
+			exclude: /(node_modules|bower_components)/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-env']
+				}
+			}
+		},
+		{
+			test: /\.css$/,
+			exclude: /node_modules/,
+			// chained loaders:
+			// style-loader injects css imported by css-loader
+			loader: 'style-loader!css-loader'
+		},
+		{
+			test: /\.html$/,
+			exclude: /node_modules/,
+			// creates String for importing into JS
+			loader: 'html-loader'
+		}
+		]
+	}
 };
