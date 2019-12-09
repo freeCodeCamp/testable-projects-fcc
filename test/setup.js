6e933b7e18c1c30f86ee338da6dfe24c1e212b4c
Kristofer Koishigawa <scissorsneedfoodtoo@gmail.com>
2019-12-10 07:11:02 +0900

    fix: Typo and minor bugfix (#1017)
    Changed example.env to sample.env to match docs and main repo. Also modified browserMaxWidth and browserMaxHeight in test/setup.js to parse strings from the .env file into numbers to prevent errors.

--- test/setup.js
+++ test/setup.js
@@ -3,8 +3,8 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const browserPath = process.env.BROWSER_PATH || '';
 const screenshotDir = process.env.SCREENSHOT_DIR || 'test/screenshots';
-const browserMaxWidth = process.env.BROWSER_MAX_WIDTH || 1600;
-const browserMaxHeight = process.env.BROWSER_MAX_HEIGHT || 900;
+const browserMaxWidth = Number(process.env.BROWSER_MAX_WIDTH) || 1600;
+const browserMaxHeight = Number(process.env.BROWSER_MAX_HEIGHT) || 900;
 
 // headless mode is enabled by default
 const headless = process.env.HEADLESS !== '0';
