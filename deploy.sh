d3732c5fc995917508fc32ed5af1c6c090a35596
Mrugesh Mohapatra <1884376+raisedadead@users.noreply.github.com>
2020-09-25 03:29:02 +0530

    chore: update deployment script

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,8 @@ then
   # Commit and push changes to CDN repo
   cd cdn/
   git add .
-  git -c user.name="$CDN_USER" -c user.email='travis' \
-    commit -m 'Travis CI testable-projects-fcc'
+  git -c user.name="$CDN_USER" -c user.email='camperbot@users.noreply.github.com' \
+    commit -m 'chore(travis-CI): update testable-projects-fcc bundle'
   git push https://"$CDN_USER":"$CDN_API_KEY"@github.com/"$CDN_REPO_SLUG" master
 fi
 popd
