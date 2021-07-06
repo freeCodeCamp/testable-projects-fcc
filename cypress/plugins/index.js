d7b08c3edede18626a1aef2b7597a391bcc5dfb1
Sem Bauke <semboot699@gmail.com>
2021-07-06 12:32:03 +0200

    fix: disable on uncaught exception

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,2 +1,9 @@
 /* eslint-disable no-unused-vars */
-module.exports = function (on, config) {};
+module.exports = function (on, config) {
+    on('task', {
+        log(message) {
+            console.log(message);
+            return message;
+        }
+    })
+};
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -21,12 +21,12 @@ Cypress.Commands.add('checkProjectTests', (projectPath) => {
           .find('.test.fail')
           .each(($el) => {
             const text = $el.get(0).innerText;
-            cy.task('log', text);
+            cy.task('log', text).then((result) => {
+              // fail Cypress test
+              throw new Error('Error: ' + result);
+            });;
           })
-          .then(() => {
-            // fail Cypress test
-            throw new Error();
-          });
+          
       }
     });
 });
