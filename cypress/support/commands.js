04814aa2deb92083ca9477c705060070be67808c
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2021-07-22 16:13:49 +0900

    fix: add timeout to allow page and remote data to load

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,6 +3,15 @@
 Cypress.Commands.add('checkProjectTests', (projectPath) => {
   cy.visit(projectPath);
 
+  // Wait for the page to finish loading. In some cases, just for example the
+  // D3 projects, projects are loading remote data, so we have to be generous
+  // here.
+  // TODO: A better way to do this is to change all of the example projects to
+  // set a variable such as 'fccTestableProjectsDataLoaded', and then we can
+  // wait for that variable to be true. But that will require changing every
+  // example project.
+  cy.wait(2000);
+
   cy.get('#fcc_test_suite_wrapper')
     .shadow()
     .find('#fcc_test_message-box-rerun-button')
