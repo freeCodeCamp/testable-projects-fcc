82544b0cf9297901e98c6dc557ca8bad62b58a3c
Sem Bauke <46919888+Sembauke@users.noreply.github.com>
2021-07-26 17:39:48 +0200

    feat: init cypress on testable projects (#1258)
    * feat: init cypress on testable projects
    
    * feat: add custom commands and extra test
    
    * fix: linting
    
    * feat: run projects in a suite
    
    * test: cypress workflow
    
    * fix: rebase with upstream, install packages, remove package-lock.json
    
    * fix: lint generate-fixture.js
    
    * fix: remove update file
    
    * test: workflow test cypress
    
    * test: workflow test cypress 2
    
    * test: workflow test cypress 3
    
    * test: workflow test cypress 4
    
    * test: workflow test cypress 5
    
    * test: workflow test cypress 6
    
    * fix(workflow): change name
    
    * fix: spelling mistake
    
    * fix: run fixture command via node instead of cypress
    
    * fix: use start instead of build
    
    * test: cypress workflow
    
    * fix: workfow
    
    * Update .github/workflows/cypress-firefox.yml
    
    Co-authored-by: Kristofer Koishigawa <scissorsneedfoodtoo@gmail.com>
    
    * Update .github/workflows/cypress-firefox.yml
    
    Co-authored-by: Kristofer Koishigawa <scissorsneedfoodtoo@gmail.com>
    
    * fix: update cypress firefox test
    
    * fix: update cypress chrome test
    
    * fix: disable on uncaught exception
    
    * fix: linting
    
    * fix: add timeout to allow page and remote data to load
    
    * fix: cypress.json spacing
    
    * feat: set max width and height for cypress
    
    * fix: ignore cypress videos and screenshots
    
    * delete: selenium workflow
    
    * Apply suggestions from code review
    
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
    
    * fix: workflow
    
    * fix: test spec
    
    * fix: commands
    
    * fix: remove fixture file as it gets automatically updated
    
    * fix: generate directory
    
    Co-authored-by: Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

---
+++ cypress/support/commands.js
@@ -0,0 +1,45 @@
+/* global cy Cypress */
+
+Cypress.Commands.add('checkProjectTests', (projectPath) => {
+  cy.viewport(
+    Number(process.env.BROWSER_MAX_WIDTH) || 1600,
+    Number(process.env.BROWSER_MAX_HEIGHT) || 900
+  );
+
+  cy.visit(projectPath);
+
+  // Wait for the page to finish loading. In some cases, just for example the
+  // D3 projects, projects are loading remote data, so we have to be generous
+  // here.
+  // TODO: A better way to do this is to change all of the example projects to
+  // set a variable such as 'fccTestableProjectsDataLoaded', and then we can
+  // wait for that variable to be true. But that will require changing every
+  // example project.
+  cy.wait(2000);
+
+  cy.get('#fcc_test_suite_wrapper')
+    .shadow()
+    .find('#fcc_test_message-box-rerun-button')
+    .click();
+
+  cy.get('#fcc_test_suite_wrapper')
+    .shadow()
+    .find('.fcc_test_btn-done', { timeout: 60000 })
+    .click();
+
+  cy.get('#fcc_test_suite_wrapper')
+    .shadow()
+    .then(($wrapper) => {
+      if ($wrapper.find('.test.fail').length) {
+        cy.wrap($wrapper)
+          .find('.test.fail')
+          .each(($el) => {
+            const text = $el.get(0).innerText;
+            cy.task('log', text).then((result) => {
+              // fail Cypress test
+              throw new Error('Error: ' + result);
+            });
+          });
+      }
+    });
+});
---
+++ generate-fixture.js
@@ -0,0 +1,34 @@
+const { writeFileSync, readdirSync, mkdir } = require('fs');
+const path = require('path');
+
+function createProjectFixtures() {
+  const projectDir = readdirSync(
+    path.join(__dirname, './src/projects'),
+    'utf-8'
+  );
+
+  let projectObject = {};
+
+  mkdir(path.join(__dirname, './cypress/fixtures/'), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+
+  projectDir.forEach((project) => {
+    // Remove excessive files
+    if (project === 'local-test' || project === 'index.pug') {
+      return;
+    }
+    projectObject[project] = '/pages/' + project;
+  });
+
+  writeFileSync(
+    path.join('./cypress/fixtures/project-fix.json'),
+    JSON.stringify(projectObject, null, 2)
+  );
+
+  return projectObject;
+}
+
+createProjectFixtures();
